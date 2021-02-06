ad171da1b644b993e6d7ac61fca7786409f1f6b5
martins.jekabovics <martins.jekabovics@whitedigital.eu>
2021-02-06 15:52:49 +0200

    Setted up FE side of the project

---
+++ app/Http/Controllers/Api/MovieController.php
@@ -0,0 +1,22 @@
+<?php
+
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use GuzzleHttp;
+
+
+class MovieController extends Controller
+{
+    public function getTopMovies()
+    {
+        $client = new GuzzleHttp\Client(['base_uri' => env('MY_API_FILMS_DOMAIN')]);
+
+        $response = $client->get('/top',[
+            'token' =>  env('MY_API_FILMS_TOKEN')
+        ]);
+
+        return $response->getBody();
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+Route::prefix('movies')->group(function () {
+    Route::get('/top', 'App\Http\Controllers\Api@getTopMovies');
 });
