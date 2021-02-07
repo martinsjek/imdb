5aea2e42070c1e2a01f392c125d71a00fb74e95e
martins.jekabovics <martins.jekabovics@whitedigital.eu>
2021-02-07 15:35:09 +0200

    Added migration command to startup execution file

--- docker/startup.sh
+++ docker/startup.sh
@@ -3,6 +3,7 @@
 service php7.4-fpm start
 service cron start
 cd /var/www/html && /usr/bin/php artisan movies:top
+cd /var/www/html && /usr/bin/php artisan migrate
 cd /var/www/html && /usr/bin/php artisan serve --host=0.0.0.0 --port=8000
 nginx -g "daemon off;"
 
